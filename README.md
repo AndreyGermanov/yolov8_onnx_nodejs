d87054a99d6b4d930407c1268c06e80515f36192
Andrey Germanov <andrey@germanov.dev>
2023-05-09 18:08:37 +0300

    Implementation

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# yolov8_onnx_nodejs
-YOLOv8 inference using Node.js
+# YOLOv8 inference using Node.js
+
+This is a web interface to [YOLOv8 object detection neural network](https://ultralytics.com/yolov8)
+implemented on [Node.js](https://www.nodejs.org).
+
+## Install
+
+* Clone this repository: `git clone git@github.com:AndreyGermanov/yolov8_onnx_nodejs.git`
+* Go to the root of cloned repository
+* Install dependencies by running `npm install`
+
+## Run
+
+Execute:
+
+```
+node object_detector.js
+```
+
+It will start a webserver on http://localhost:8080. Use any web browser to open the web interface.
+
+Using the interface you can upload the image to the object detector and see bounding boxes of all objects detected on it.
---
+++ object_detector.js
@@ -0,0 +1,180 @@
+const ort = require("onnxruntime-node");
+const express = require('express');
+const multer = require("multer");
+const sharp = require("sharp");
+const fs = require("fs");
+
+/**
+ * Main function that setups and starts a
+ * web server on port 8080
+ */
+function main() {
+    const app = express();
+    const upload = multer();
+
+    /**
+     * The site root handler. Returns content of index.html file.
+     */
+    app.get("/", (req,res) => {
+        res.end(fs.readFileSync("index.html", "utf8"))
+    })
+
+    /**
+     * The handler of /detect endpoint that receives uploaded
+     * image file, passes it through YOLOv8 object detection network and returns
+     * an array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..] as a JSON
+     */
+    app.post('/detect', upload.single('image_file'), async function (req, res) {
+        const boxes = await detect_objects_on_image(req.file.buffer);
+        res.json(boxes);
+    });
+
+    app.listen(8080, () => {
+        console.log(`Server is listening on port 8080`)
+    });
+}
+
+/**
+ * Function receives an image, passes it through YOLOv8 neural network
+ * and returns an array of detected objects and their bounding boxes
+ * @param buf Input image body
+ * @returns Array of bounding boxes in format [[x1,y1,x2,y2,object_type,probability],..]
+ */
+async function detect_objects_on_image(buf) {
+    const [input,img_width,img_height] = await prepare_input(buf);
+    const output = await run_model(input);
+    return process_output(output,img_width,img_height);
+}
+
+/**
+ * Function used to convert input image to tensor,
+ * required as an input to YOLOv8 object detection
+ * network.
+ * @param buf Content of uploaded file
+ * @returns Array of pixels
+ */
+async function prepare_input(buf) {
+    const img = sharp(buf);
+    const md = await img.metadata();
+    const [img_width,img_height] = [md.width, md.height];
+    const pixels = await img.removeAlpha()
+        .resize({width:640,height:640,fit:'fill'})
+        .raw()
+        .toBuffer();
+    const red = [], green = [], blue = [];
+    for (let index=0; index<pixels.length; index+=3) {
+        red.push(pixels[index]/255.0);
+        green.push(pixels[index+1]/255.0);
+        blue.push(pixels[index+2]/255.0);
+    }
+    const input = [...red, ...green, ...blue];
+    return [input, img_width, img_height];
+}
+
+/**
+ * Function used to pass provided input tensor to YOLOv8 neural network and return result
+ * @param input Input pixels array
+ * @returns Raw output of neural network as a flat array of numbers
+ */
+async function run_model(input) {
+    const model = await ort.InferenceSession.create("yolov8m.onnx");
+    input = new ort.Tensor(Float32Array.from(input),[1, 3, 640, 640]);
+    const outputs = await model.run({images:input});
+    return outputs["output0"].data;
+}
+
+/**
+ * Function used to convert RAW output from YOLOv8 to an array of detected objects.
+ * Each object contain the bounding box of this object, the type of object and the probability
+ * @param output Raw output of YOLOv8 network
+ * @param img_width Width of original image
+ * @param img_height Height of original image
+ * @returns Array of detected objects in a format [[x1,y1,x2,y2,object_type,probability],..]
+ */
+async function process_output(output, img_width, img_height) {
+    let boxes = [];
+    for (let index=0;index<8400;index++) {
+        const [class_id,prob] = [...Array(8400-4).keys()]
+            .map(col => [col, output[8400*(col+4)+index]])
+            .reduce((accum, item) => item[1]>accum[1] ? item : accum,[0,0]);
+        if (prob < 0.5) {
+            continue;
+        }
+        const label = yolo_classes[class_id];
+        const xc = output[index];
+        const yc = output[8400+index];
+        const w = output[8400*2+index];
+        const h = output[8400*3+index];
+        const x1 = (xc-w/2)/640*img_width;
+        const y1 = (yc-h/2)/640*img_height;
+        const x2 = (xc+w/2)/640*img_width;
+        const y2 = (yc+h/2)/640*img_height;
+        boxes.push([x1,y1,x2,y2,label,prob]);
+    }
+
+    boxes = boxes.sort((box1,box2) => box2[5]-box1[5])
+    const result = [];
+    while (boxes.length>0) {
+        result.push(boxes[0]);
+        boxes = boxes.filter(box => iou(boxes[0],box)<0.7);
+    }
+    return result;
+}
+
+/**
+ * Function calculates "Intersection-over-union" coefficient for specified two boxes
+ * https://pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/.
+ * @param box1 First box in format: [x1,y1,x2,y2,object_class,probability]
+ * @param box2 Second box in format: [x1,y1,x2,y2,object_class,probability]
+ * @returns Intersection over union ratio as a float number
+ */
+const iou = (box1,box2) => intersect(box1,box2)/union(box1,box2);
+
+/**
+ * Function calculates union area of two boxes.
+ *     :param box1: First box in format [x1,y1,x2,y2,object_class,probability]
+ *     :param box2: Second box in format [x1,y1,x2,y2,object_class,probability]
+ *     :return: Area of the boxes union as a float number
+ * @param box1 First box in format [x1,y1,x2,y2,object_class,probability]
+ * @param box2 Second box in format [x1,y1,x2,y2,object_class,probability]
+ * @returns Area of the boxes union as a float number
+ */
+const union = (box1,box2) => {
+    const [box1_x1,box1_y1,box1_x2,box1_y2] = box1;
+    const [box2_x1,box2_y1,box2_x2,box2_y2] = box2;
+    const box1_area = (box1_x2-box1_x1)*(box1_y2-box1_y1)
+    const box2_area = (box2_x2-box2_x1)*(box2_y2-box2_y1)
+    return box1_area + box2_area - intersect(box1,box2)
+}
+
+/**
+ * Function calculates intersection area of two boxes
+ * @param box1 First box in format [x1,y1,x2,y2,object_class,probability]
+ * @param box2 Second box in format [x1,y1,x2,y2,object_class,probability]
+ * @returns Area of intersection of the boxes as a float number
+ */
+const intersect = (box1,box2) => {
+    const [box1_x1,box1_y1,box1_x2,box1_y2] = box1;
+    const [box2_x1,box2_y1,box2_x2,box2_y2] = box2;
+    const x1 = Math.max(box1_x1,box2_x1);
+    const y1 = Math.max(box1_y1,box2_y1);
+    const x2 = Math.min(box1_x2,box2_x2);
+    const y2 = Math.min(box1_y2,box2_y2);
+    return (x2-x1)*(y2-y1)
+}
+
+/**
+ * Array of YOLOv8 class labels
+ */
+const yolo_classes = [
+    'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat',
+    'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
+    'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase',
+    'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard',
+    'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple',
+    'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant',
+    'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven',
+    'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+];
+
+main()
